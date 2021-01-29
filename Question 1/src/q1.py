d211b67f1f80ac96b5f13b981ff34becd63cc453
icecoffee <yixuanyu98@gmail.com>
2021-01-29 21:00:51 +0800

    q1 half finished

--- Question 1/src/q1.py
+++ Question 1/src/q1.py
@@ -1,29 +1,73 @@
-
 # %%
+''' Get the route from the top left corner to the bottom right corner as a list
+
+The idea of a function is to go from the bottom right corner
+to the top right corner. The option is either 'up' or 'right'.
+Save each of the step in a list, and finally reverse the list as a return. 
+For each of the step, say the current point is (i, j), consider
+the matrix above: (i-1)*j. If the maximum number that this matrix can 
+provide is smaller than the remaining part of sum, goes right, otherwise, 
+goes up until it arrives the point of (1, 1). This is because the value
+in the same line could provide a relative lager figure to be minus by the 
+remaning part of sum.
+
+Args:
+    m: total lines of the matrix
+    n: total columns of the matrix
+    q_sum: sum that needs to be added
+
+Return:
+    the list of route that records the direction for each step. For example:
+
+    ['D', 'R', 'R', 'R', 'D', 'D', 'D']
+
+'''
 
 
 def get_route(m, n, q_sum):
-    t_sum = q_sum
+    # create a list to store the operation
+    # the 'up' operation in the reversed direction corresponds to 
+    # the 'down' operation in the original direction
+    # the 'left' operation in the reversed direction corresponds to 
+    # the 'right' operation in the original direction
     step_list = []
-    t_sum -= m
-    max_val = 0.5 * m * (m - 1) + (m - 1) * (n - 1)  # 能加上的最大值
+
+    # from the bottom right corner to the top right corner, the bottom right corner
+    # is the first and must-pass point, so q_sum should minus this point first.
+    q_sum -= m
+
+    # calculate the maximum the matrix above can provide
+    max_val = 0.5 * m * (m - 1) + (m - 1) * (n - 1)
     while not (m == 1 and n == 1):
-        if t_sum > max_val:  # 如果上方矩阵能提供的最大值小于剩下的sum，再本行还要再加一次
-            t_sum -= m
+        # If the maximum number that this matrix can 
+        # provide is smaller than the remaining part of sum, goes left
+        # otherwise, goes up. 
+        if q_sum > max_val: 
+            q_sum -= m
             n -= 1
             step_list.append('R')
         else:
-            t_sum -= m - 1
+            q_sum -= m - 1
             m -= 1
             step_list.append('D')
-        max_val = 0.5 * m * (m - 1) + (m - 1) * (n - 1)  # 能加上的最大值
+        
+        # after moving forward, recalculate the maximum the matrix above can provide
+        # because the matrix above is changed
+        max_val = 0.5 * m * (m - 1) + (m - 1) * (n - 1) 
 
-    # finally reverse the list to the right order
+    # finally reverse the list to the right order and return
     reversed_step_list = list(reversed(step_list))
     return reversed_step_list
 
 
 # %%
+'''Write the outputs into the file
+
+Args:
+    q_sum: sum that needs to be added
+    list: the list of operation
+
+'''
 
 
 def write_output(q_sum, list):
@@ -52,4 +96,8 @@ list4 = get_route(9, 9, 100)
 print(100, ''.join(list4))
 write_output(100, list4)
 
+# %%
+# The number is too large. Sorry, I haven't find a solution yet
+# list5 = get_route(90000, 100000, 87127231192)
+# print(87127231192, ''.join(list5))
 # %%
